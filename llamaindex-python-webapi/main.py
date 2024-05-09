f94efef0fe1199c00bbb354869e203fc2898c874
Anthony Chu <anthony@anthonychu.ca>
2024-05-08 18:47:41 -0700

    Update LlamaIndex sample

--- llamaindex-python-webapi/main.py
+++ llamaindex-python-webapi/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 import dotenv
 from fastapi.responses import RedirectResponse
 from llama_index.llms.azure_openai import AzureOpenAI
-from llama_index.tools.azure_dynamic_sessions import AzureDynamicSessionsToolSpec
+from llama_index.tools.azure_code_interpreter import AzureCodeInterpreterToolSpec
 from llama_index.core.agent import ReActAgent
 
 dotenv.load_dotenv()
@@ -15,22 +15,21 @@ app = FastAPI()
 pool_management_endpoint = os.getenv("POOL_MANAGEMENT_ENDPOINT")
 
 
-cached_token = None
-credential = DefaultAzureCredential()
-def azure_ad_token_provider():
-    global cached_token
-    if not cached_token or datetime.fromtimestamp(cached_token.expires_on, timezone.utc) < (datetime.now(timezone.utc) + timedelta(minutes=5)):
-        cached_token = credential.get_token("https://cognitiveservices.azure.com/.default")
-    return cached_token.token
-
-
 @app.get("/")
 async def root():
-    return RedirectResponse("/chat?message=what is the current date and time?")
+    return RedirectResponse("/docs")
 
 
 @app.get("/chat")
 async def chat(message: str):
+    cached_token = None
+    credential = DefaultAzureCredential()
+    def azure_ad_token_provider():
+        nonlocal cached_token
+        if not cached_token or datetime.fromtimestamp(cached_token.expires_on, timezone.utc) < (datetime.now(timezone.utc) + timedelta(minutes=5)):
+            cached_token = credential.get_token("https://cognitiveservices.azure.com/.default")
+        return cached_token.token
+
     llm = AzureOpenAI(
         model="gpt-35-turbo",
         deployment_name="gpt-35-turbo",
@@ -40,13 +39,14 @@ async def chat(message: str):
         use_azure_ad=True,
     )
 
-    dynamic_session_tool = AzureDynamicSessionsToolSpec(
+    code_interpreter_tool = AzureCodeInterpreterToolSpec(
         pool_managment_endpoint=pool_management_endpoint,
     )
-    agent = ReActAgent.from_tools(dynamic_session_tool.to_tool_list(), llm=llm, verbose=True)
+    agent = ReActAgent.from_tools(code_interpreter_tool.to_tool_list(), llm=llm, verbose=True)
+    chat_response = agent.chat(message)
 
     response = {
-        "output": agent.chat(message)
+        "output": chat_response.response,
     }
 
     return response
