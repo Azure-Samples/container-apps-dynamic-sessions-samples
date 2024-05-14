298f350d2d085170e8906a13ff1fcccb16c58c7d
Bagatur <baskaryan@gmail.com>
2024-05-14 09:24:03 -0700

    langchain: use tool-calling agent

--- langchain-python-webapi/main.py
+++ langchain-python-webapi/main.py
@@ -17,37 +17,33 @@ app = FastAPI()
 async def root():
     return RedirectResponse("/docs")
 
+
 @app.get("/chat")
 async def chat(message: str):
     credential = DefaultAzureCredential()
-    os.environ["OPENAI_API_TYPE"] = "azure_ad"
-    os.environ["OPENAI_API_KEY"] = credential.get_token("https://cognitiveservices.azure.com/.default").token
     pool_management_endpoint = os.getenv("POOL_MANAGEMENT_ENDPOINT")
+    openai_api_key = credential.get_token(
+        "https://cognitiveservices.azure.com/.default"
+    ).token
 
     llm = AzureChatOpenAI(
+        api_key=openai_api_key,
         azure_deployment="gpt-35-turbo",
         openai_api_version="2024-02-01",
-        streaming=True,
+        openai_api_type="azure_ad",
         temperature=0,
     )
 
-    repl = SessionsPythonREPLTool(
-        pool_management_endpoint=pool_management_endpoint,
-    )
+    repl = SessionsPythonREPLTool(pool_management_endpoint=pool_management_endpoint)
 
     tools = [repl]
-    react_agent = agents.create_react_agent(
-        llm=llm,
-        tools=tools,
-        prompt=hub.pull("hwchase17/react"),
+    prompt = hub.pull("hwchase17/openai-functions-agent")
+    agent = agents.create_tool_calling_agent(llm, tools, prompt)
+
+    agent_executor = agents.AgentExecutor(
+        agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
     )
 
-    react_agent_executor = agents.AgentExecutor(
-        agent=react_agent, tools=tools, verbose=True, handle_parsing_errors=True
-    )
+    response = agent_executor.invoke({"input": message})
 
-    response = react_agent_executor.invoke({"input": message})
-
-    return {
-        "output": response["output"],
-    }
+    return {"output": response["output"]}
