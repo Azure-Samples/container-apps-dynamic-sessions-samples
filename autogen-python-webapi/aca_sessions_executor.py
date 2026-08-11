408d97bd7e76d228f50d8ba3e961ccbc9d7a8b47
Jessica Petty <jepetty@microsoft.com>
2026-08-11 15:28:42 -0700

    Refactor chat setup, add token expiration

--- autogen-python-webapi/aca_sessions_executor.py
+++ autogen-python-webapi/aca_sessions_executor.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime, timedelta, timezone
 from typing import List
 from azure.identity import DefaultAzureCredential
 from autogen.coding import CodeBlock, CodeExecutor, CodeExtractor, CodeResult, MarkdownCodeExtractor
@@ -13,14 +14,22 @@ class ACASessionsExecutor(CodeExecutor):
         self.pool_management_endpoint = pool_management_endpoint
         self.session_identifier = session_identifier
         self.access_token = None
+        self.access_token_expires_on = None
 
     def ensure_access_token(self) -> None:
-        if not self.access_token:
+        refresh_before = datetime.now(timezone.utc) + timedelta(minutes=5)
+        if (
+            not self.access_token
+            or not self.access_token_expires_on
+            or datetime.fromtimestamp(self.access_token_expires_on, timezone.utc) < refresh_before
+        ):
             credential = DefaultAzureCredential(
                 exclude_shared_token_cache_credential=True
             )
             scope = "https://dynamicsessions.io/.default"
-            self.access_token = credential.get_token(scope).token
+            access_token = credential.get_token(scope)
+            self.access_token = access_token.token
+            self.access_token_expires_on = access_token.expires_on
 
     def execute_code_blocks(self, code_blocks: List[CodeBlock]) -> CodeResult:
         self.ensure_access_token()
